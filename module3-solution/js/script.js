0077fd490ca0a066c3f0ed8248b242dbf3cd2498
Serban Patriciu <serban_patriciu@hotmail.com>
2019-01-07 21:53:18 -0500

    Am renuntat la ascunderea meniului.

--- module3-solution/js/script.js
+++ module3-solution/js/script.js
@@ -1,7 +1,11 @@
 $( document ).ready(() => {
     console.log("A01");
-   $("#toggle-button").blur(function(event) {
-       console.log("A02");
-       $("#collapsable-nav").collapse('hide');
-   }) 
+//    $("#toggle-button").blur(function(event) {
+//        console.log("A02");
+//        var screenWidth = window.innerWidth;
+
+//        if (screenWidth < 768) {
+//         $("#collapsable-nav").collapse('hide');
+//        }
+//     }) 
 });
